9d8602a0582f70a4533540ff177d95d5d47a2e9d
Brady Holt <brady.holt@gmail.com>
2019-04-19 17:40:41 -0500

    Do not ignore nodule_modules

--- bin/start.js
+++ bin/start.js
@@ -2,7 +2,11 @@
 
 "use strict";
 
-require("ts-node").register({ transpileOnly: true, typeCheck: false });
+require("ts-node").register({
+  transpileOnly: true,
+  typeCheck: false, // By default ts-node ignores .ts files in node_modules
+  ignore: []
+});
 const cli = require("../src/cli.ts").init(process.cwd(), process.argv.slice(2));
 
 (async function() {
