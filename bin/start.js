6790d065d07e47c3c22fb490a87a1540081b7640
Brady Holt <brady.holt@gmail.com>
2019-04-19 15:57:20 -0500

    test npx

--- bin/start.js
+++ bin/start.js
@@ -2,7 +2,6 @@
 
 "use strict";
 
-console.log("HELLO")
 require("ts-node").register({ transpileOnly: true, typeCheck: false });
 const cli = require("../src/cli.ts").init(process.cwd(), process.argv.slice(2));
 
